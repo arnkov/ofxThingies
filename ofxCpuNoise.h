d34cc9850f6a47ec5ae2fe777f7bad696440a13d
Arne Koenig <82873244+arnkov@users.noreply.github.com>
2023-12-12 01:00:38 +0100

    Update ofxCpuNoise.h

--- ofxCpuNoise.h
+++ ofxCpuNoise.h
@@ -8,8 +8,7 @@ struct ofxCpuNoise {
 		height = h;
 	}
 	
-	void update() {
-		velocity = ofGetElapsedTimef()*0.5f;
+	void update(float velocity) {
 		ofPixels& pix = img.getPixels();
 		for(int y=0; y<height; y++) {
 			for(int  x=0; x<width; x++) {
